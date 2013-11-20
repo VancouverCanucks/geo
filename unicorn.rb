1bb9099fe761230865406d322537b58361bc6285
Mike Timofiiv <mike.timofiiv@gmail.com>
2013-11-20 09:36:47 -0800

    unicorn config stability enchancements

--- unicorn.rb
+++ unicorn.rb
@@ -1,12 +1,27 @@
 @dir = File.expand_path(File.join(File.dirname(__FILE__), '.'))
 
-worker_processes 5
+worker_processes 10
 working_directory @dir
 
-timeout 30
+timeout 90
 
-listen "#{@dir}/tmp/sockets/unicorn.sock", :backlog => 64
+listen "#{@dir}/tmp/sockets/unicorn.sock", :backlog => 2048
 pid "#{@dir}/tmp/pids/unicorn.pid"
 
 stderr_path "#{@dir}/log/unicorn.stderr.log"
 stdout_path "#{@dir}/log/unicorn.stdout.log"
+
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
